7e4266c1bf971156b233eae26b4c653a6ff1d35f
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2022-10-29 18:27:45 +0400

    new version2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = service.calculate (price);
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
